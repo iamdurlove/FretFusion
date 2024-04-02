ff98dc170dc6a981820635e7578b77338da4a0de
Durlav Parajuli <durlove60@gmail.com>
2024-04-02 16:04:50 +0545

    project structured

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,4 +25,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/about', [SiteController::class, 'about'])->name('about');
+
+require __DIR__ . '/auth.php';
